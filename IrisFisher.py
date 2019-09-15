952a83a04c4417ee1fdb558407de550a20f04f0e
potap995 <potap995@gmail.com>
2019-09-15 14:06:18 +0300

    new struct

--- IrisFisher.py
+++ IrisFisher.py
@@ -16,17 +16,3 @@ def softmax_grad(x):
     x = x.reshape(3, 1)
     return np.diagflat(x) - np.dot(x, x.T)
 
-def forward_prop(X, y, W, A):
-    A[0] = np.dot(X, W[0])
-    A[1] = softmax(A[0])
-    L = logloss(A[1], y)
-
-def back_prop(X, y, W, A, giper_params):
-    dLdP = logloss_grad(A[1], y).reshape(1, 3) #1x3
-    dPda = softmax_grad(A[1]) # 3x3
-    dLda = np.dot(dLdP, dPda).T #3x1
-    print(dLda, X)
-    print("--------------------------------------------------------------")
-    dLdW = np.dot(dLda, X)
-    W[0] -= giper_params["learning rate"] * dLdW
-
