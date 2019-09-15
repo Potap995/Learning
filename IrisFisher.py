0ca2c7f5b589bcba4fa0f5bfa7ec5134307eb0ea
potap995 <potap995@gmail.com>
2019-09-15 18:09:52 +0300

    back prop

--- IrisFisher.py
+++ IrisFisher.py
@@ -6,13 +6,13 @@ def softmax(x):
     return exps / np.sum(exps)
 
 def logloss(x, y):
-    return - np.dot(y, np.log(x))
+    return - np.dot(y, np.log(x).T)
 
 def logloss_grad(x, y):
     return -(y / x)
 
 def softmax_grad(x):
     # Let x is col
-    x = x.reshape(3, 1)
-    return np.diagflat(x) - np.dot(x, x.T)
+    assert x.shape == (1, 3)
+    return np.diagflat(x) - np.dot(x.T, x)
 
