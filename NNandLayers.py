acb0a23c8a445aa27c689914402d043b516c9461
potap995 <potap995@gmail.com>
2019-11-14 16:11:26 +0300

    Add Classes

---
+++ NNandLayers.py
@@ -0,0 +1,59 @@
+class FullyConnectedLayer:
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class ReLULayer:
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class SigmoidLayer:
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class SoftMaxLayer:
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class NET:
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def eval(self):
+        pass
+
