c80c42201d48befcb5f7074b4e4958608af33eb1
potap995 <potap995@gmail.com>
2019-11-14 21:58:28 +0300

    forward pass

--- NNandLayers.py
+++ NNandLayers.py
@@ -1,24 +1,23 @@
+import numpy as np
+
+def random_init(size, left=-0.1, right=0.1):
+    return np.random.random(size=size) * (right - left) + left
+
 class FullyConnectedLayer:
 
-    def __init__(self):
-        pass
+    def __init__(self, in_shape, out_shape, **params):
+        self.in_shape = in_shape
+        self.out_shape = out_shape
+        self.lr = params.pop("learning_rate", 0.01)
+        self.w = random_init((in_shape, out_shape))
+        self.b = np.zeros((1, out_shape))
 
-    def forward(self):
-        pass
+    def forward(self, data):
+        self.in_data = data
+        self.out_data = np.dot(data, self.w) + self.b
+        return self.out_data
 
-    def backward(self):
-        pass
-
-
-class ReLULayer:
-
-    def __init__(self):
-        pass
-
-    def forward(self):
-        pass
-
-    def backward(self):
+    def backward(self, deriv):
         pass
 
 
@@ -27,10 +26,12 @@ class SigmoidLayer:
     def __init__(self):
         pass
 
-    def forward(self):
-        pass
+    def forward(self, data):
+        self.in_data = data
+        self.out_data =  1. / (1. + np.exp(-data))
+        return self.out_data
 
-    def backward(self):
+    def backward(self, deriv):
         pass
 
 
@@ -39,10 +40,10 @@ class SoftMaxLayer:
     def __init__(self):
         pass
 
-    def forward(self):
+    def forward(self, data):
         pass
 
-    def backward(self):
+    def backward(self, deriv):
         pass
 
 
