57c032d707d1ee253734a28a0ffdf2e69b257e89
potap995 <potap995@gmail.com>
2019-11-15 20:05:49 +0300

    Add FC and Sigmoid backward

--- NNandLayers.py
+++ NNandLayers.py
@@ -9,16 +9,22 @@ class FullyConnectedLayer:
         self.in_shape = in_shape
         self.out_shape = out_shape
         self.lr = params.pop("learning_rate", 0.01)
-        self.w = random_init((in_shape, out_shape))
+        self.batch_size = params.pop("batch_size", 1)
+        self.W = random_init((in_shape, out_shape))
         self.b = np.zeros((1, out_shape))
 
     def forward(self, data):
         self.in_data = data
-        self.out_data = np.dot(data, self.w) + self.b
-        return self.out_data
+        return np.dot(data, self.w) + self.b
 
-    def backward(self, deriv):
-        pass
+    def backward(self, loss):
+        grad_b = np.sum(loss, axis=0) / self.batch_size
+        grad_W = np.dot(self.in_data.T, loss)
+
+        self.W -= self.lr * grad_W
+        self.b -= self.lr * grad_b
+
+        return np.dot(loss, self.W.T)
 
 
 class SigmoidLayer:
@@ -28,11 +34,12 @@ class SigmoidLayer:
 
     def forward(self, data):
         self.in_data = data
-        self.out_data =  1. / (1. + np.exp(-data))
+        self.out_data = 1. / (1. + np.exp(-data))
         return self.out_data
 
-    def backward(self, deriv):
-        pass
+    def backward(self, loss):
+        deriv = self.out_data(1 - self.out_data)
+        return loss * deriv
 
 
 class SoftMaxLayer:
